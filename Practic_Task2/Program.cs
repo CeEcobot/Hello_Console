40802e15b7726fd8d0a83ec581735b29268c09ee
Efimtsev_Denis <efimtsev.denis@gmail.com>
2022-12-11 05:06:31 +0300

    Add square a & b

---
+++ Practic_Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое целое число");
+int numa = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int numb = Convert.ToInt32(Console.ReadLine());
+int square = numb * numb;
+if (square == numa)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+Console.WriteLine("Первое число не является квадратом второго числа");
