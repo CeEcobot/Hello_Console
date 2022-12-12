8ad889efb104bd56ad8ddd16000fe856d2e7b67b
Efimtsev_Denis <efimtsev.denis@gmail.com>
2022-12-12 07:46:13 +0300

    Add Practic Task 4

---
+++ Practic_Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int num2 = num1 * -1;
+while(num2 <= num1) //вывод всех чисел от введенного до num2
+{
+Console.Write($"{num2} ");
+num2++;
+}
+Console.WriteLine();
